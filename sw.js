0a31ebdfa624c190d1823c43251aff5d02f3c5a6
Brammmetje <Bram.kerkman@gmail.com>
2026-07-25 23:13:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Cache dient alleen als offline-terugval (handig op de baan zonder bereik).
 // Verhoog VERSION bij elke nieuwe versie (gelijk aan APP_VERSION in index.html).
 
-const VERSION = "5";
+const VERSION = "10";
 const CACHE = "slagvaardig-v" + VERSION;
 
 self.addEventListener("install", (event) => {
